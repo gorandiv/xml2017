0c431009a1e395338b2ca8db8cf535da4de72b0f
lukasicar <sicluka@yahoo.com>
2017-06-23 20:32:47 +0200

    Web servis jedan prosao

---
+++ Bank/src/main/java/bank/proba/Test.java
@@ -0,0 +1,5 @@
+package bank.proba;
+
+public class Test {
+
+}
---
+++ Company/src/main/java/company/test/CountryEndpoint.java
@@ -0,0 +1,31 @@
+package company.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import io.spring.guides.gs_producing_web_service.Country;
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "country")
+	@ResponsePayload
+	public Country getCountry(@RequestPayload Country request) {
+		Country c = new Country();
+		System.out.println("jebes mi mater");
+		return c;
+	}
+}
